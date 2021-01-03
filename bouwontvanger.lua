03b5b7f410cfa1ff4a1151063eba9b34bb553b76
Boricraft-Development <48557560+Boricraft-Development@users.noreply.github.com>
2021-01-03 18:00:28 +0100

    fixing error

--- bouwontvanger.lua
+++ bouwontvanger.lua
@@ -2,7 +2,7 @@ function setup ()
   rednet.open("left")
   id,message = rednet.receive()
   if id == 1 and message == "conn_test" then
-  rednet.broadcast("systems on computer " + os.getComputerID() + " are ready")
+  rednet.broadcast("systems on computer " .. os.getComputerID() .. " are ready")
   end
 end
 
